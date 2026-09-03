4420f94b80b0e042ac997ff178ce5b967ed1f5fe
Nicolle Daniela Valverde Gallego <nicolle.valverde01@uceva.edu.co>
2026-09-02 22:14:28 -0500

    fix: eliminar referencia a sistema.scss

--- src/app/modules/sistema/sistema.ts
+++ src/app/modules/sistema/sistema.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-sistema',
   standalone: false,
   templateUrl: './sistema.html',
-  styleUrl: './sistema.scss',
 })
 export class Sistema {
 
